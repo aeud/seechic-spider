6b7eab06bf37a837d73620cad0dc4ba323d57ae2
aeud <adrien.eudes@gmail.com>
2015-12-03 16:17:14 +0800

    update

--- app2.js
+++ app2.js
@@ -6,7 +6,7 @@ var parseString = require('xml2js').parseString
 
 request.get('http://sg.seechic.com/sitemap.xml', function(err, resp, body){
     parseString(body, function (err, res) {
-        console.log('Crawling ' + res.urlset.url.length ' pages...')
+        console.log('Crawling ' + res.urlset.url.length + ' pages...')
         async.parallel(res.urlset.url.map(row => {
             return (callback) => {
                 var url = row.loc[0]
